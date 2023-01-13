1ed46eae48db01ea5564befc2080748741c17bfa
DESKTOP-OR0EAIA\Escal <escalonaf12@gmail.com>
2023-01-13 00:45:48 -0400

    archivos varios

---
+++ variables.py
@@ -0,0 +1,11 @@
+my_name=input("cual es tu nombre")
+
+print(f"mi nombre es  {my_name}")
+
+dato1=input("Numero 1: ")
+dato2=input("Numero 2 ")
+
+operacion=int(dato1)+int(dato2)
+
+print(f"La operacion realizada por {my_name} es {operacion}")
+
